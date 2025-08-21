f45509b22a7963467db377f108b41f536f261ef3
jukaruba51 <jukaruba@gmail.com>
2025-08-20 20:53:06 -0700

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,8 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
 resource "aws_instance" "myec2" {
-    ami = "ami-00c39f71452c088333"
+    ami = "ami-020cba7c55df1f615"
     instance_type = "t2.micro"
 }
